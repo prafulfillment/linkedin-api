0dd723465b1b2409ac8583d265d5f05c2355ec9b
Praful Mathur <praful.mathur@gmail.com>
2013-03-07 17:17:58 -0500

    Patching requests oauth2 for linkedin

--- requests_oauth2/oauth2.py
+++ requests_oauth2/oauth2.py
@@ -36,10 +36,12 @@ class OAuth2(object):
         """
         Requests an access token
         """
-        url = "%s%s" % (self.site, quote(self.token_url))
         data = {'redirect_uri': self.redirect_uri, 'client_id': self.client_id, 'client_secret': self.client_secret, 'code': code}
         data.update(kwargs)
-        response = requests.post(url, data=data)
+
+        url = "%s%s?%s" % (self.site, quote(self.token_url), urlencode(data))
+
+        response = requests.post(url)
 
         if isinstance(response.content, basestring):
             try:
