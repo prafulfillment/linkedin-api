66a45064d1983525754389ca3d7581c66361662e
Praful Mathur <praful.mathur@gmail.com>
2013-03-13 15:58:08 -0400

    Made a property, PEP8, fixed silly mistake.

--- linkedin_requests_oauth2/linkedin_auth.py
+++ linkedin_requests_oauth2/linkedin_auth.py
@@ -11,18 +11,36 @@ class LinkedIn_OAuth2(OAuth2):
     response_type = 'code'
     grant_type = 'authorization_code'
 
-    def __init__(self, client_id, client_secret, redirect_uri):
-        self.state = self.unique_string()
-        OAuth2.__init__(self, client_id, client_secret, LINKEDIN_URL, redirect_uri, AUTHORIZATION_URL, TOKEN_URL)
+    def __init__(self,
+                client_id, client_secret,
+                redirect_uri, site=LINKEDIN_URL,
+                authorization_url=AUTHORIZATION_URL, token_url=TOKEN_URL):
+        self._state = self.unique_string()
 
-    def unique_string(size=20, valid_chars=None):
+        OAuth2.__init__(self,
+                client_id=client_id, client_secret=client_secret,
+                site=LINKEDIN_URL, redirect_uri=redirect_uri,
+                authorization_url=authorization_url, token_url=token_url)
+
+    @property
+    def state(self):
+        return self._state
+
+    def unique_string(self, size=20, valid_chars=None):
         if valid_chars is None:
             valid_chars = string.letters + string.digits
 
-        return ''.join(random.choice(valid_chars) for x in range(20))
+        return ''.join(random.choice(valid_chars) for x in range(size))
 
     def authorize_url(self, scope='', **kwargs):
-        return OAuth2.authorize_url(self, scope=scope, state=self.state, response_type=self.response_type)
+        state = self.state
+        response_type = self.response_type
+
+        return OAuth2.authorize_url(self,
+                scope=scope, state=state, response_type=response_type,
+                **kwargs)
 
     def get_token(self, code, **kwargs):
-        return OAuth2.get_token(self, code, grant_type=self.grant_type)
+        return OAuth2.get_token(self,
+                code=code, grant_type=self.grant_type,
+                **kwargs)
